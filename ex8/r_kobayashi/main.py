31ea21b9c90a7c055602b1c283ba42a7c22ff608
r_kobayashi <kobayashi.ren@g.sp.m.is.nagoya-u.ac.jp>
2022-06-29 10:35:46 +0900

    docstring追加

--- ex8/r_kobayashi/main.py
+++ ex8/r_kobayashi/main.py
@@ -1,16 +1,16 @@
 import argparse
 import os
-from tracemalloc import stop
+
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
-from functools import wraps
-import time
-import math
-import decimal
-from sklearn.metrics import confusion_matrix
 import seaborn as sns
+import time
+from functools import wraps
+from tracemalloc import stop
+import sklearn.metrics as skl
+
 
 def stop_watch(func):
     @wraps(func)
@@ -24,70 +24,150 @@ def stop_watch(func):
     return wrapper
 
 
-def round_num(x, degit):
-    x_degits = math.floor(math.log10(abs(x)))
-    x_rounded = decimal.Decimal(str(x)).quantize(decimal.Decimal(str(10 ** (x_degits - degit + 1))), rounding = "ROUND_HALF_UP")
-    return x_rounded
-
-
 class HMM:
-    def __init__(self, a, b, pi, k, m):
+    def __init__(self, k, m, a, b, pi):
+        """ 
+        Parameter
+        ---------
+        k : int
+            number of status of latent variable
+        m : int
+            number of status of output variable
+        a : np.ndarray(k, k) 
+            state transition probability distribution
+        b : np.ndarray(k,m)
+            Output probability distribution
+        pi : np.ndarray(k, 1)
+            initial state probability distribution
+        """
+        self.k = k
+        self.m = m
         self.a = a
         self.b = b
         self.pi = pi
-        self.k = k
-        self.m = m
+        
 
-    @stop_watch
-    def forward(self, output, samplesize):
-        # alpha = np.zeros(samplesize)
+    # @stop_watch
+    def forward(self, n_out, size_out, output):
+        """ 
+        Forward algorithm
+        
+        Parameter
+        ---------
+        n_out : int
+            number of output series
+        size_out : int
+            size of output series
+        output : np.ndarray(n_out, size_out)
+            output series
+        
+        Return
+        ------
+        prob : np.ndarray(100,)
+
+        """
         # 初期化
-        alpha = self.b[:, output[:, 0]].T.reshape(samplesize, self.k, 1)
-        for i in range(samplesize):
+        alpha = self.b[:, output[:, 0]].T.reshape(n_out, self.k, 1)
+        for i in range(n_out):
                 alpha[i, :, :] = alpha[i, :, :] * self.pi
         # 再帰的計算
-        for j in range(self.m):
-            for i in range(samplesize):
+        for j in range(size_out):
+            for i in range(n_out):
                 alpha[i, :, :] = (alpha[i, :, :].T @ self.a).T
-            alpha = self.b[:, output[:, j]].T.reshape(100, 3, 1) * alpha
+            alpha = self.b[:, output[:, j]].T.reshape(n_out, self.k, 1) * alpha
         # 確率計算
-        P = np.zeros(samplesize)
-        for i in range(samplesize):
-            P[i] = np.sum(alpha[i, :, :])
-        return P
+        prob = np.zeros(n_out)
+        for i in range(n_out):
+            prob[i] = np.sum(alpha[i, :, :])
+        return prob
 
-    @stop_watch
-    def viterbi(self, output, samplesize):
+    # @stop_watch
+    def viterbi(self, n_out, size_out, output):
+        """ 
+        Viterbi algorithm
+        
+        Parameter
+        ---------
+        n_out : int
+            number of output series
+        size_out : int
+            size of output series
+        output : np.ndarray(n_out, size_out)
+            output series
+        
+        Return
+        ------
+        prob : np.ndarray(100,)
+         """
         # 初期化
-        psi = self.b[:, output[:, 0]].T.reshape(100, 3, 1)
-        for i in range(samplesize):
+        psi = self.b[:, output[:, 0]].T.reshape(n_out, self.k, 1)
+        for i in range(n_out):
             psi[i, :, :] = psi[i, :, :] * self.pi
         # 再帰的計算
-        max = np.zeros((samplesize, self.k, 1))
-        PSI = np.zeros((samplesize, self.m, self.k))
-        for j in range(self.m):
-            for i in range(samplesize):
+        max = np.zeros((n_out, self.k, 1))
+        PSI = np.zeros((n_out, size_out, self.k))
+        for j in range(size_out):
+            for i in range(n_out):
                 for k in range(self.k):
                     # k: 次の状態
-                    # PSI[k]: 次の状態がkとなる確率最大
-                    tmp = psi[i, :, :] * self.a[:, k].reshape(3, 1)
+                    # PSI[k]: 次の状態がkである確率が最大となる状態
+                    tmp = psi[i, :, :] * self.a[:, k].reshape(self.k, 1)
                     max[i, k, :] = np.max(tmp)
                     PSI[i, j, k] = np.argmax(tmp)
                     psi[i, k, :] = self.b[int(PSI[i, j, k]), output[i, j]] * max[i, k]
         # 終了
-        P = np.zeros(samplesize)
-        for i in range(samplesize):
-            P[i] = np.max(psi[i, :, :])
-        return PSI, P
-        # 系列の復元
+        prob = np.zeros(n_out)
+        for i in range(n_out):
+            prob[i] = np.max(psi[i, :, :])
+        return prob
 
+
+def viterbi_plus(self, n_out, size_out, output):
+        """ 
+        Viterbi algorithm
+        
+        Parameter
+        ---------
+        n_out : int
+            number of output series
+        size_out : int
+            size of output series
+        output : np.ndarray(n_out, size_out)
+            output series
+        
+        Return
+        ------
+        prob : np.ndarray(100,)
+         """
+        # 初期化
+        psi = self.b[:, output[:, 0]].T.reshape(n_out, self.k, 1)
+        for i in range(n_out):
+            psi[i, :, :] = psi[i, :, :] * self.pi
+        # 再帰的計算
+        max = np.zeros((n_out, self.k, 1))
+        PSI = np.zeros((n_out, size_out, self.k))
+        for j in range(size_out):
+            for i in range(n_out):
+                
+                for k in range(self.k):
+                    # k: 次の状態
+                    # PSI[k]: 次の状態がkである確率が最大となる状態
+                    tmp = psi[i, :, :] * self.a[:, k].reshape(self.k, 1)
+                    max[i, k, :] = np.max(tmp)
+                    PSI[i, j, k] = np.argmax(tmp)
+                    psi[i, k, :] = self.b[int(PSI[i, j, k]), output[i, j]] * max[i, k]
+        # 終了
+        prob = np.zeros(n_out)
+        for i in range(n_out):
+            prob[i] = np.max(psi[i, :, :])
+        return prob
 
 def main(args):
+    # データの読み込み
     path = os.path.dirname(__file__)
     fname = os.path.join(path, "data", args.fname)
     graph_title = os.path.splitext(args.fname)[0]
     data = pickle.load(open(fname, "rb"))
-
     answer = np.array(data['answer_models'])
     a = np.array(data['models']['A'])
     b = np.array(data['models']['B'])
@@ -95,42 +175,42 @@ def main(args):
     output = np.array(data['output'])
     modelsize = pi.shape[0]
     k = pi.shape[1]
-    m = output.shape[1]
-    samplesize = output.shape[0]
-
+    m = b.shape[1]
+    n_out = output.shape[0]
+    size_out = output.shape[1]
+    # 確率の計算
     models = []
-    P_forward = np.zeros((modelsize, samplesize))
-    PSI = np.zeros((modelsize, samplesize, m, k))
-    P_viterbi = np.zeros((modelsize, samplesize))
+    prob_forward = np.zeros((modelsize, n_out))
+    # PSI = np.zeros((modelsize, n_out, m, k))
+    prob_viterbi = np.zeros((modelsize, n_out))
     for i in range(modelsize):
-        models.append(HMM(a[i], b[i], pi[i], k, m))
-        P_forward[i] = models[i].forward(output, samplesize)
-        PSI[i], P_viterbi[i] = models[i].viterbi(output, samplesize)
-    print(">>> print(PSI)")
-    print(PSI)
-    P_forward = P_forward.T
-    P_viterbi = P_viterbi.T
-    print(">>> print(P_viterbi)")
-    print(P_viterbi)
-    pred_forward = np.zeros(samplesize)
-    pred_viterbi = np.zeros(samplesize)
-    for i in range(samplesize):
-        pred_forward[i] = np.argmax(P_forward[i, :])
-        pred_viterbi[i] = np.argmax(P_viterbi[i, :])
-    print(">>> print(pred_forward)")
-    print(pred_forward)
-    print(">>> print(pred_viterbi)")
-    print(pred_viterbi)
-    cm_forward = confusion_matrix(answer, pred_forward)
-    cm_viterbi = confusion_matrix(answer, pred_viterbi)
-    # plot
+        models.append(HMM(k, m, a[i], b[i], pi[i]))
+        prob_forward[i] = models[i].forward(n_out, size_out, output)
+        prob_viterbi[i] = models[i].viterbi(n_out, size_out, output)
+    prob_forward = prob_forward.T
+    prob_viterbi = prob_viterbi.T
+    # 生成元のモデルを予測
+    pred_forward = np.zeros(n_out)
+    pred_viterbi = np.zeros(n_out)
+    for i in range(n_out):
+        pred_forward[i] = np.argmax(prob_forward[i, :])
+        pred_viterbi[i] = np.argmax(prob_viterbi[i, :])
+    # 結果のプロット
+    cm_forward = skl.confusion_matrix(answer, pred_forward)
+    acc_forward = skl.accuracy_score(answer, pred_forward)
+    cm_viterbi = skl.confusion_matrix(answer, pred_viterbi)
+    acc_viterbi = skl.accuracy_score(answer, pred_viterbi)
     fig, ax = plt.subplots(1, 2, figsize=(8, 6))
     fig.subplots_adjust(wspace=0.5)
-    sns.heatmap(cm_forward, annot=True, cmap='Greys', ax=ax[0])
-    ax[0].set(xlabel="predict model", ylabel="answer model", title=f"{graph_title} Forward")
-    sns.heatmap(cm_viterbi, annot=True, cmap='Greys', ax=ax[1])
-    ax[1].set(xlabel="predict model", ylabel="answer model", title=f"{graph_title} Viterbi")
-    plt.savefig(os.path.join(path, "figs", "result.png"))
+    sns.heatmap(cm_forward, annot=True, cmap='Blues', ax=ax[0])
+    ax[0].set(xlabel="predict model",
+              ylabel="answer model",
+              title=f"{graph_title} Forward\n(acc = {acc_forward})",)
+    sns.heatmap(cm_viterbi, annot=True, cmap='Blues', ax=ax[1])
+    ax[1].set(xlabel="predict model",
+              ylabel="answer model",
+              title=f"{graph_title} Viterbi\n(acc = {acc_viterbi})",)
+    plt.savefig(os.path.join(path, "figs", graph_title+".png"))
     plt.show()
 
 if __name__ == "__main__":
