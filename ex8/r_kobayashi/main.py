9b3a28e0b7b70aaa53bed6cd3cd1ab704669e0d3
r_kobayashi <kobayashi.ren@g.sp.m.is.nagoya-u.ac.jp>
2022-06-29 12:10:40 +0900

    計算時間を測定できるように修正

--- ex8/r_kobayashi/main.py
+++ ex8/r_kobayashi/main.py
@@ -1,14 +1,12 @@
 import argparse
 import os
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import seaborn as sns
 import time
 from functools import wraps
-from tracemalloc import stop
 import sklearn.metrics as skl
 
 
@@ -47,7 +45,6 @@ class HMM:
         self.pi = pi
         
 
-    # @stop_watch
     def forward(self, n_out, size_out, output):
         """ 
         Forward algorithm
@@ -81,7 +78,7 @@ class HMM:
             prob[i] = np.sum(alpha[i, :, :])
         return prob
 
-    # @stop_watch
+
     def viterbi(self, n_out, size_out, output):
         """ 
         Viterbi algorithm
@@ -122,45 +119,21 @@ class HMM:
         return prob
 
 
-def viterbi_plus(self, n_out, size_out, output):
-        """ 
-        Viterbi algorithm
-        
-        Parameter
-        ---------
-        n_out : int
-            number of output series
-        size_out : int
-            size of output series
-        output : np.ndarray(n_out, size_out)
-            output series
-        
-        Return
-        ------
-        prob : np.ndarray(100,)
-         """
-        # 初期化
-        psi = self.b[:, output[:, 0]].T.reshape(n_out, self.k, 1)
-        for i in range(n_out):
-            psi[i, :, :] = psi[i, :, :] * self.pi
-        # 再帰的計算
-        max = np.zeros((n_out, self.k, 1))
-        PSI = np.zeros((n_out, size_out, self.k))
-        for j in range(size_out):
-            for i in range(n_out):
-                
-                for k in range(self.k):
-                    # k: 次の状態
-                    # PSI[k]: 次の状態がkである確率が最大となる状態
-                    tmp = psi[i, :, :] * self.a[:, k].reshape(self.k, 1)
-                    max[i, k, :] = np.max(tmp)
-                    PSI[i, j, k] = np.argmax(tmp)
-                    psi[i, k, :] = self.b[int(PSI[i, j, k]), output[i, j]] * max[i, k]
-        # 終了
-        prob = np.zeros(n_out)
-        for i in range(n_out):
-            prob[i] = np.max(psi[i, :, :])
-        return prob
+    @stop_watch
+    def run_forward(models, n_model, n_out, size_out, output):
+        prob = np.zeros((n_model, n_out))
+        for i in range(n_model):
+            prob[i] = models[i].forward(n_out, size_out, output)
+        return prob.T
+
+
+    @stop_watch
+    def run_viterbi(models, n_model, n_out, size_out, output):
+        prob = np.zeros((n_model, n_out))
+        for i in range(n_model):
+            prob[i] = models[i].viterbi(n_out, size_out, output)
+        return prob.T
+
 
 def main(args):
     # データの読み込み
@@ -173,22 +146,22 @@ def main(args):
     b = np.array(data['models']['B'])
     pi = np.array(data['models']['PI'])
     output = np.array(data['output'])
-    modelsize = pi.shape[0]
+    """ print(a.shape)
+    print(b.shape)
+    print(pi.shape) """
+    n_model = pi.shape[0]
     k = pi.shape[1]
     m = b.shape[1]
     n_out = output.shape[0]
     size_out = output.shape[1]
     # 確率の計算
     models = []
-    prob_forward = np.zeros((modelsize, n_out))
-    # PSI = np.zeros((modelsize, n_out, m, k))
-    prob_viterbi = np.zeros((modelsize, n_out))
-    for i in range(modelsize):
+    prob_forward = np.zeros((n_model, n_out))
+    prob_viterbi = np.zeros((n_model, n_out))
+    for i in range(n_model):
         models.append(HMM(k, m, a[i], b[i], pi[i]))
-        prob_forward[i] = models[i].forward(n_out, size_out, output)
-        prob_viterbi[i] = models[i].viterbi(n_out, size_out, output)
-    prob_forward = prob_forward.T
-    prob_viterbi = prob_viterbi.T
+    prob_forward = HMM.run_forward(models, n_model, n_out, size_out, output)
+    prob_viterbi = HMM.run_viterbi(models, n_model, n_out, size_out, output)
     # 生成元のモデルを予測
     pred_forward = np.zeros(n_out)
     pred_viterbi = np.zeros(n_out)
