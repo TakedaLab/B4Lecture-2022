67a4cc7ec9a1eb2cd13fedbe68576abadc07a2b8
tahayakawa <hayakawa.tatsuya@g.sp.m.is.nagoya-u.ac.jp>
2022-07-12 20:45:12 +0900

    train, test 修正

--- ex9/t_hayakawa/main.py
+++ ex9/t_hayakawa/main.py
@@ -26,13 +26,15 @@ class Sound_Data(Dataset):
         sr = 44100
         # 最長音声の長さ
         if phase == "train":
-            self.maxLength = np.max([len(self.data[i]) for i in range(self.len)])
+            self.maxLength = np.max([len(self.data[i])
+                                    for i in range(self.len)])
         else:
             self.maxLength = maxLength
 
         # zero padding & mel_spectrogram
         for i in tqdm(range(self.len)):
-            self.data[i] = np.pad(self.data[i], (0, self.maxLength - len(self.data[i])))
+            self.data[i] = np.pad(
+                self.data[i], (0, self.maxLength - len(self.data[i])))
             mel_spec = librosa.feature.melspectrogram(y=self.data[i], sr=sr)
             self.data[i] = librosa.power_to_db(mel_spec, ref=np.max)
 
@@ -72,6 +74,52 @@ class CNN(nn.Module):
         return x
 
 
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    running_loss = 0
+    correct = 0
+    for (inputs, labels) in dataloader:
+        inputs, labels = inputs[:, np.newaxis, :, :].to(
+            device), labels.to(device)
+
+        pred = model(inputs)
+        loss = loss_fn(pred, labels)
+        running_loss += loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        correct += (pred.argmax(1) == labels).type(torch.float).sum().item()
+    train_loss = running_loss / size
+    correct = correct / size
+    print(
+        f"Training: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {train_loss:>8f} \n"
+    )
+    return correct
+
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for inputs, labels in dataloader:
+            inputs, labels = (
+                inputs[:, np.newaxis, :, :].to(device),
+                labels.to(device),
+            )
+            pred = model(inputs)
+            test_loss += loss_fn(pred, labels).item()
+            correct += (pred.argmax(1) ==
+                        labels).type(torch.float).sum().item()
+    test_loss /= size
+    correct /= size
+    print(
+        f"Test: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
+    )
+    return correct
+
+
 def main():
     meta_train_file = "training.csv"
     meta_test_file = "test_truth.csv"
@@ -104,63 +152,21 @@ def main():
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-    def train(dataloader, model, loss_fn, optimizer):
-        size = len(dataloader.dataset)
-        running_loss = 0
-        correct = 0
-        for (inputs, labels) in dataloader:
-            inputs, labels = inputs[:, np.newaxis, :, :].to(device), labels.to(device)
-
-            pred = model(inputs)
-            loss = loss_fn(pred, labels)
-            running_loss += loss
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            correct += (pred.argmax(1) == labels).type(torch.float).sum().item()
-        train_loss = running_loss / size
-        correct = correct / size
-        print(
-            f"Training: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {train_loss:>8f} \n"
-        )
-        return correct
-
-    def test(dataloader, model):
-        size = len(dataloader.dataset)
-        model.eval()
-        test_loss, correct = 0, 0
-        with torch.no_grad():
-            for inputs, labels in dataloader:
-                inputs, labels = (
-                    inputs[:, np.newaxis, :, :].to(device),
-                    labels.to(device),
-                )
-                pred = model(inputs)
-                test_loss += loss_fn(pred, labels).item()
-                correct += (pred.argmax(1) == labels).type(torch.float).sum().item()
-        test_loss /= size
-        correct /= size
-        print(
-            f"Test: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
-        )
-        return correct
-
     epochs = 100
     train_corrects = np.zeros(epochs)
     test_corrects = np.zeros(epochs)
     for t in range(epochs):
         print(f"Epoch {t+1}\n------------------------")
-        train_corrects[t] = train(train_loader, model, loss_fn, optimizer)
-        test_corrects[t] = test(test_loader, model)
+        train_corrects[t] = train(
+            train_loader, model, loss_fn, optimizer, device)
+        test_corrects[t] = test(test_loader, model, loss_fn, device)
     print("Done!")
 
     fig = plt.figure()
     plt.plot(np.arange(epochs), train_corrects, c="blue", label="train")
     plt.plot(np.arange(epochs), test_corrects, c="orange", label="test")
     plt.title(
-        f"result\nAccuracy(Training): {(100*train_corrects[t]):>0.1f}%\nAccuracy(Test): {(100*test_corrects[t]):>0.1f}%"
-    )
+        f"result\nAccuracy(Training): {(100*train_corrects[t]):>0.1f}%\nAccuracy(Test): {(100*test_corrects[t]):>0.1f}%")
     plt.xlabel("epochs")
     plt.ylabel("accuracy")
     plt.legend()
@@ -170,4 +176,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
